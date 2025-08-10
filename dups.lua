e2df2b28806a68901ae291a10b7e266c848fbbfe
shqieg <kseniahrolina@gmail.com>
2025-08-10 14:30:48 +0700

    Update dups.lua

--- dups.lua
+++ dups.lua
@@ -1,28 +1,55 @@
+local scriptActive = false
+local menuVisible = false
+local buttonRadius = 30
+local buttonPosX, buttonPosY = 50, 50
+
 function main()
     while true do
         wait(0)
-        local w, h = getScreenResolution()
-        drawRect(0, 0, w, h, 0x90000000)
-        drawBorder(w/2-150, h/2-100, 300, 200, 1, 0xFFFFFFFF)
+        
+        -- Рисуем круглую кнопку активации
+        drawCircle(buttonPosX, buttonPosY, buttonRadius, 0xFF000000) -- Черный круг
+        drawText("G", buttonPosX - 5, buttonPosY - 10, 0xFFFFFFFF, 1.0) -- Белая буква G
+        
+        -- Если меню видимо - рисуем черный фон
+        if menuVisible then
+            local screenX, screenY = getScreenResolution()
+            drawRect(0, 0, screenX, screenY, 0x90000000) -- Полупрозрачный черный фон
+        end
+        
+        -- Проверка клика по кнопке
+        if isMouseClicked() then
+            local mouseX, mouseY = getMousePos()
+            if (mouseX - buttonPosX)^2 + (mouseY - buttonPosY)^2 <= buttonRadius^2 then
+                menuVisible = not menuVisible
+                scriptActive = not scriptActive
+            end
+        end
+    end
+end
+
+-- Вспомогательные функции
+function drawCircle(x, y, radius, color)
+    for angle = 0, 360, 1 do
+        local rad = math.rad(angle)
+        local px = x + math.cos(rad) * radius
+        local py = y + math.sin(rad) * radius
+        drawRect(px, py, px + 1, py + 1, color)
     end
 end
 
 function drawRect(x1, y1, x2, y2, color)
-    imgui.ImGui.GetBackgroundDrawList():AddRectFilled(
-        imgui.ImVec2(x1, y1),
-        imgui.ImVec2(x2, y2),
-        color
-    )
+    -- Реализация рисования прямоугольника
 end
 
-function drawBorder(x, y, w, h, thickness, color)
-    local dl = imgui.ImGui.GetBackgroundDrawList()
-    -- Top
-    dl:AddLine(imgui.ImVec2(x, y), imgui.ImVec2(x+w, y), color, thickness)
-    -- Right
-    dl:AddLine(imgui.ImVec2(x+w, y), imgui.ImVec2(x+w, y+h), color, thickness)
-    -- Bottom
-    dl:AddLine(imgui.ImVec2(x, y+h), imgui.ImVec2(x+w, y+h), color, thickness)
-    -- Left
-    dl:AddLine(imgui.ImVec2(x, y), imgui.ImVec2(x, y+h), color, thickness)
+function drawText(text, x, y, color, scale)
+    -- Реализация рисования текста
+end
+
+function isMouseClicked()
+    -- Проверка клика мыши
+end
+
+function getMousePos()
+    -- Получение позиции мыши
 end
